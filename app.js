3f82fb95c493c97c02712ee530a12b9c76b76b41
Junjun Li <junjunli666@gmail.com>
2019-01-10 12:34:23 +0800

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const os = require('os');
 
-console.log("...");
+console.log("he"...");
 var handler = function(request,response) {
  console.log("Received request from "+request.connection.remoteAddress);
  response.writeHead(200);
